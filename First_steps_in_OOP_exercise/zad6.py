a5ee30c6146524ae11b5abeba9e65c11a0547307
Nikolay Marinov <nik.marinov09@gmail.com>
2025-11-03 22:31:08 +0200

    Exercise 6 initial commit

---
+++ First_steps_in_OOP_exercise/zad6.py
@@ -0,0 +1,24 @@
+class SteamUser:
+    def __init__(self, username: str, games: list[str]):
+        self.username = username
+        self.games = games
+        self.played_hours: int = 0
+
+    def play(self, game: str, hours: int) -> str:
+        if game not in self.games:
+            return f"{game} is not in library"
+
+        self.played_hours += hours
+
+        return f"{self.username} is playing {game}"
+
+    def buy_game(self, game: str) -> str:
+        if game in self.games:
+            return f"{game} is already in your library"
+
+        self.games.append(game)
+
+        return f"{self.username} bought {game}"
+
+    def status(self) -> str:
+        return f"{self.username} has {len(self.games)} games. Total play time: {self.played_hours}"
