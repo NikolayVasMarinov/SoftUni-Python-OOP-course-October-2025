1ac1d62d455eff4fda7b3b2e25bb202b30be25d7
Nikolay Marinov <nik.marinov09@gmail.com>
2025-11-09 22:29:21 +0200

    Exercise 6 initial commit

---
+++ Inheritance/zad6.py
@@ -0,0 +1,19 @@
+class Stack:
+    def __init__(self):
+        self.data: list = []
+
+    def push(self, element) -> None:
+        self.data.append(element)
+
+    def pop(self):
+        return self.data.pop()
+
+    def top(self):
+        return self.data[-1]
+
+    def is_empty(self) -> bool:
+        if self.data: return False
+        return True
+
+    def __str__(self):
+        return f"[{', '.join(self.data[::-1])}]"
