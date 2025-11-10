f2b55d8bb88d2709d56f3eb7bbf2b6613ace3bec
Nikolay Marinov <nik.marinov09@gmail.com>
2025-11-10 13:38:36 +0200

    Inheritance exercise: Exercise 4 initial commit

---
+++ Inheritance_exercise/zad4/project/vehicle.py
@@ -0,0 +1,13 @@
+class Vehicle:
+    DEFAULT_FUEL_CONSUMPTION: float = 1.25
+
+    def __init__(self, fuel: float, horse_power: int):
+        self.fuel_consumption: float = self.DEFAULT_FUEL_CONSUMPTION
+        self.fuel: float = fuel
+        self.horse_power: int = horse_power
+
+    def drive(self, kilometers: float) -> None:
+        needed_fuel = self.fuel_consumption * kilometers
+
+        if self.fuel >= needed_fuel:
+            self.fuel -= needed_fuel
