967fb5fc3b3379d1be9229212ce89b97c739ea22
Nikolay Marinov <nik.marinov09@gmail.com>
2025-11-13 23:33:50 +0200

    Encapsulation: Exercise 3 password validation touch-ups

--- Encapsulation/zad3.py
+++ Encapsulation/zad3.py
@@ -21,21 +21,17 @@ class Profile:
 
     @password.setter
     def password(self, password: str):
-        upper_present = False
-        digit_present = False
-        if 8 <= len(password):
-            for c in password:
-                if c.isdigit():
-                    digit_present = True
-
-                if c.isupper():
-                    upper_present = True
+        upper_present = any(c.isdigit() for c in password)
+        digit_present = any(c.isupper() for c in password)
+        if 8 <= len(password) and upper_present and digit_present:
 
             if upper_present and digit_present:
                 self.__password = password
                 return
 
-        raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+        raise ValueError("The password must be 8 or more characters with at least "
+                         "1 digit and 1 uppercase letter.")
 
     def __str__(self):
-        return f'You have a profile with username: "{self.username}" and password: {"*" * len(self.password)}'
+        return (f'You have a profile with username: "{self.username}" '
+                f'and password: {"*" * len(self.password)}')
