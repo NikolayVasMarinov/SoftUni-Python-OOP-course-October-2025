2a451d0a6695faa9aa78adbe61622bc2e6558de3
Nikolay Marinov <nik.marinov09@gmail.com>
2025-11-20 20:58:51 +0200

    Polymorphism and abstraction exercise: Exercise 1 initial commit

---
+++ Polymorphism_and_abstraction_exercise/zad1.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    def __init__(self, fuel_quantity: float, fuel_consumption: float):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    @abstractmethod
+    def drive(self, distance: float) -> None:
+        pass
+
+    @abstractmethod
+    def refuel(self, fuel: float) -> None:
+        pass
+
+
+class Car(Vehicle):
+    EXTRA_CONSUMPTION = 0.9
+
+    def __init__(self, fuel_quantity: float, fuel_consumption: float):
+        super().__init__(fuel_quantity, fuel_consumption + self.EXTRA_CONSUMPTION)
+
+    def drive(self, distance: float) -> None:
+        needed = distance * self.fuel_consumption
+
+        if needed <= self.fuel_quantity:
+            self.fuel_quantity -= needed
+
+    def refuel(self, fuel: float) -> None:
+        self.fuel_quantity += fuel
+
+
+class Truck(Vehicle):
+    EXTRA_CONSUMPTION = 1.6
+    REFUEL_EFFICIENCY = 0.95
+
+    def __init__(self, fuel_quantity: float, fuel_consumption: float):
+        super().__init__(fuel_quantity, fuel_consumption + self.EXTRA_CONSUMPTION)
+
+    def drive(self, distance: float) -> None:
+        needed = distance * self.fuel_consumption
+
+        if needed <= self.fuel_quantity:
+            self.fuel_quantity -= needed
+
+    def refuel(self, fuel: float) -> None:
+        self.fuel_quantity += fuel * self.REFUEL_EFFICIENCY
