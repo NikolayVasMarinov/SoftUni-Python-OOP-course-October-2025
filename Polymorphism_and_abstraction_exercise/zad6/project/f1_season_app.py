bf90e4dee6db6ab7d4d93ec1e739dd6d145748ac
Nikolay Marinov <nik.marinov09@gmail.com>
2025-11-22 20:26:24 +0200

    Polymorphism and abstraction exercise: Removed team_name setter and fixed small mistake in if statement for checking if teams are registered in exercise 6

--- Polymorphism_and_abstraction_exercise/zad6/project/f1_season_app.py
+++ Polymorphism_and_abstraction_exercise/zad6/project/f1_season_app.py
@@ -14,21 +14,13 @@ class F1SeasonApp:
         elif team_name == "Mercedes":
             self.mercedes_team = MercedesTeam(budget)
 
-        return f"{team_name} has joined the new F1 season."
-
-    @property
-    def team_name(self):
-        return self.__team_name
-
-    @team_name.setter
-    def team_name(self, team_name: str):
-        if team_name not in ["Red Bull", "Mercedes"]:
+        else:
             raise ValueError("Invalid team name!")
 
-        self.__team_name = team_name
+        return f"{team_name} has joined the new F1 season."
 
     def new_race_results(self, race_name: str, red_bull_pos: int, mercedes_pos: int) -> str:
-        if self.red_bull_team and self.mercedes_team is None:
+        if self.red_bull_team is None and self.mercedes_team is None:
             raise Exception("Not all teams have registered for the season.")
 
         return (f"Red Bull: {self.red_bull_team.calculate_revenue_after_race(red_bull_pos)}. "
