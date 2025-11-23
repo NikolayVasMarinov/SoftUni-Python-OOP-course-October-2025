bf47e4460398c3e7c5960e191ebbdd6db6db5e73
Nikolay Marinov <nik.marinov09@gmail.com>
2025-11-23 22:38:06 +0200

    Iterators and generators: Exercise 1 using generator

--- Iterators_and_generators/zad1.py
+++ Iterators_and_generators/zad1.py
@@ -1,18 +1,8 @@
 class custom_range:
     def  __init__(self, start: int, end: int):
-        self.current = start
+        self.start = start
         self.end = end
 
-#    def __iter__(self):
-   #     for num in range(self.start, self.end + 1):
-      #      yield num
-
     def __iter__(self):
-        return self
-
-    def __next__(self):
-        self.current += 1
-        if self.current <= self.end:
-            return self.current
-        else:
-            raise StopIteration
+        for num in range(self.start, self.end + 1):
+            yield num
