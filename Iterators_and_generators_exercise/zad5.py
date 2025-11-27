3758a2d4c3fabe946442cebc8e6dfe67c2e816e0
Nikolay Marinov <nik.marinov09@gmail.com>
2025-11-27 14:43:15 +0200

    Iterators and generators exercise: Exercise 5 initial commit

---
+++ Iterators_and_generators_exercise/zad5.py
@@ -0,0 +1,15 @@
+def solution():
+    def integers():
+        i = 0
+        while True:
+            i += 1
+            yield i
+
+    def halves():
+        for i in integers():
+            yield i / 2
+
+    def take(n, seq):
+        return [next(seq) for _ in range(n)]
+
+    return take, halves, integers
