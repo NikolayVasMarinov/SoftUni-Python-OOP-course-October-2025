f8d1024c719aa74d398484927d4b284522f566b5
Nikolay Marinov <nik.marinov09@gmail.com>
2025-11-30 23:29:22 +0200

    Decorators exercise: Exercise 5 initial commit

---
+++ Decorators_exercise/zad5.py
@@ -0,0 +1,22 @@
+def cache(function):
+    class Cache:
+        def __init__(self, func):
+            self.func = func
+            self.log: dict[int, int] = {}
+
+        def __call__(self, n):
+            if n in self.log:
+                return self.log[n]
+
+            result = self.func(n)
+            self.log[n] = result
+            return result
+
+    return Cache(function)
+
+@cache
+def fibonacci(n: int):
+    if n < 2:
+        return n
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
